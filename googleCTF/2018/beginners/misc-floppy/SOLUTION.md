2f98f9dc3b007426e808b4ee01b2a21ad5e4ae42
Aaron Lichtman <aaronlichtman@gmail.com>
2019-07-15 03:28:28 -0700

    Fixed quackme and added a google CTF problem and some exploit eduation work

---
+++ googleCTF/2018/beginners/misc-floppy/SOLUTION.md
@@ -0,0 +1,3 @@
+# Floppy
+
+Use binwalk to carve out extra files inside `foo.ico` with `$ binwalk -e foo.ico`, and the flag will be inside `driver.txt`.
--- picoCTF/2018/quackme/quackme_solver.py
+++ picoCTF/2018/quackme/quackme_solver.py
@@ -5,6 +5,7 @@ import pwn
 import json
 import r2pipe
 import codecs
+import binascii
 
 # Open file in r2 and perform auto-analysis
 r2 = r2pipe.open("quackme-main")
@@ -17,15 +18,13 @@ greeting = greetingJSON["string"].split("\\u000")[0].strip()
 
 # Extract sekrutBuffer
 r2.cmd("s obj.sekrutBuffer")
-buf = r2.cmd("px0")
+buf = r2.cmd("pxj 25").strip()[1:-1].split(",")
+hex_array = [int(hex_num) for hex_num in buf]
+ascii_decoded = binascii.hexlify(bytearray(hex_array)).decode('ascii')
 decode_hex = codecs.getdecoder("hex_codec")
-sekrut_buffer = decode_hex(buf.strip() + "00")[0]
+sekrut_buffer = decode_hex(ascii_decoded)[0]
 
-# XOR to get our flag, and add the last bracket (because this
-# buffer somehow extends past a NULL byte??? There's a hanging 0x5D
-# afterwards that would produce a closing bracket. When I solved this
-# manually, it was in my hex and the fully flag decrypted properly
-# *shrugs*)
+# XOR the greeting and the buffer to get the flag
 res = str(pwn.xor(greeting, sekrut_buffer))
-flag = res[2:len(sekrut_buffer) + 2] + "}"
+flag = res[2:len(sekrut_buffer) + 2]
 print("FLAG:", flag)
